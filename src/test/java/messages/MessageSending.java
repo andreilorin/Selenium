4a80cbd8043abd7c151e4371b97f9d10346047ae
andrei lorin <andreilorin@gmail.com>
2017-06-27 22:20:51 +0100

    added data provider and calls to methods that use it

--- src/test/java/messages/MessageSending.java
+++ src/test/java/messages/MessageSending.java
@@ -1,15 +1,35 @@
 package messages;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import data.SenderLombok;
 import model.MainPage;
 
 public class MessageSending {
-	
-	@Test
-	public void sendMessage() {
-		
+
+	@DataProvider(name = "test1")
+	public Object[][] createData1() {
+		SenderLombok sender = new SenderLombok();
+		sender.setName("Lorin");
+		return new Object[][] { { sender } };
+	}
+
+
+	@Test(dataProvider = "test1")
+	public void sendMessage(SenderLombok sender) {
+
 		MainPage mainPage = new MainPage();
 		mainPage.accessGoogle();
 		mainPage.accessMyPage();
+		mainPage.backPage();
+		mainPage.forwardPage();
+		
+		mainPage.enterName(sender.getName());
+		mainPage.enterEmail(sender.getEmail());
+		mainPage.enterPhone(sender.getPhone());
+		mainPage.enterMessage(sender.getMessage());
+		
+		mainPage.quit();
 	}
 }
