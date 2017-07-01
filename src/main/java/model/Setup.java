e6ae6d2f31b9f012663833ae1cc80590bb1b2417
andrei lorin <andreilorin@gmail.com>
2017-07-01 23:12:40 +0100

    created the improved Setup class

---
+++ src/main/java/model/Setup.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.BeforeClass;
+import org.testng.ITestContext;
+
+public class Setup {
+	
+	private final static Logger logger = LogManager.getLogger(Setup.class);
+	
+	@BeforeClass
+	public static void runOnceBeforeTesting(ITestContext xmlContext){
+		logger.info("starting before class");
+		
+		if (xmlContext.getCurrentXmlTest().getParameter("driver").equalsIgnoreCase("chrome")) {
+			logger.info("setting up chrome driver");
+			System.setProperty("webdriver.chrome.driver",
+					"C:/Users/Lorin/git/Selenium/drivers/chromedriver.exe");
+		} else if (xmlContext.getCurrentXmlTest().getParameter("driver").equalsIgnoreCase("firefox")) {
+			logger.info("setting up gecko driver");
+			System.setProperty("webdriver.gecko.driver",
+					".\\drivers\\geckodriver.exe");
+		} else {
+			logger.error("no driver type speciffied");
+		}
+	}
+
+}
